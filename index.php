3c07d03e51e085108c864f15c2e4cd9d0bf63e14
higashiyama-shunya <higashiyama.shunya@oysterworld.jp>
2023-05-24 19:08:05 +0900

    phpとMySQLの接続
    dockerfileを追加
    docker-compose.ymlにphpmyadmin追加

--- index.php
+++ index.php
@@ -3,22 +3,31 @@
 
 <head>
     <meta charset="UTF-8">
-    <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>php Test</title>
 </head>
 
 <body>
     <h1>オイスター掲示板</h1>
     <?php
-    date_default_timezone_set('Asia/Tokyo');
+    $dsn = 'mysql:dbname=mydb;port=3306;charset=utf8mb4;host=mysql';
+    $user = 'root';
+    $pass = 'root';
+    try {
+        $dbh = new PDO($dsn, $user, $pass);
+        echo "接続に成功しました\n";
+    } catch (PDOException $e) {
+        echo "接続に失敗しました。\n";
+        echo $e->getMessage() . "\n";
+    }
     function h($str)
     {
         return htmlspecialchars($str, ENT_QUOTES, "UTF-8");
     }
-    $name = $_POST['name'];
-    $comment = $_POST['comment'];
-    $date = $_POST['date'];
+    if (isset($name)) {
+        $name = $_POST['name'];
+        $comment = $_POST['comment'];
+        $date = $_POST['date'];
+    }
     $filename = "memo.txt";
     $datetime = date('Y/m/d H:i');
 
@@ -39,7 +48,7 @@
 
     ?>
     <form name="test_form" action="index.php" method="post">
-        <input type="hidden" name="date" value="<? echo "$datetime" ?>">
+        <input type="hidden" name="date" value="<?php echo "$datetime" ?>">
         <p>名前：<input type="text" name="name"></p>
         <p>コメント：</p>
         <p><textarea cols="80" rows="10" name="comment"></textarea></p>
